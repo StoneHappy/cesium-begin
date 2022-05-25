257d8c6851ac130e05b0210e7bac0a6a8039dada
xhguleixin123 <18017158729@163.com>
2022-05-25 15:54:21 +0800

    🎉 init: init proj

---
+++ webpack.common.js
@@ -0,0 +1,20 @@
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+    entry: {
+        main: "./src/index.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          use: ["html-loader"]
+        },
+        {
+          test: /\.(svg|png|jpg|gif)$/,
+          type: 'asset/resource'
+        }
+      ]
+    }
+};
